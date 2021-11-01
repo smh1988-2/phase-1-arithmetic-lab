2e9fa406245edb06f4afd1aaee8ca270ebcba3f6
smh1988-2 <seamhurley1988@gmail.com>
2021-11-01 15:56:03 -0700

    commit tree feb6a9f2da7674493a18bb4bde5aa76907ebf256

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+// same thing but as a function...
+// function multiply (num1, num2) {
+//     return num1 * num2
+// }
+
+// multiply(31, 2);
+
+const random = Math.floor((Math.random()) * 100);
+
+
+let num3 = 12; 
+let num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 20);
+
